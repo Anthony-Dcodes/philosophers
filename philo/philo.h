426ce69c05ae71bd866b5152842e0405a8bd8f87
Anthony-Dv <dvoracekantonin@icloud.com>
2026-02-03 22:25:00 +0100

    initial commit, add gitignore, makefile, philo.c philo.h

---
+++ philo/philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 22:16:53 by advorace          #+#    #+#             */
+/*   Updated: 2026/02/03 22:22:00 by advorace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h> // printf
+#include <stdlib.h> // malloc, free
+#include <string.h> // memset
+#include <unistd.h> // write, usleep,
+#include <sys/time.h> // gettimeofday
+
+/*
+pthread_create
+pthread_detach
+pthread_join
+pthread_mutex_init
+pthread_mutex_destroy
+pthread_mutex_lock
+pthread_mutex_unlock
+*/
+#include <pthread.h>
+
+#endif
