1c935989655898f0ec1845a2d731eba5b55ffb4d
Anthony-Dv <dvoracekantonin@icloud.com>
2026-02-04 18:50:52 +0100

    create parsing, need to add check to not overflow int

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 22:23:55 by advorace          #+#    #+#             */
-/*   Updated: 2026/02/03 22:24:18 by advorace         ###   ########.fr       */
+/*   Updated: 2026/02/04 18:46:27 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 int	main(int argc, char *argv[])
 {
-	
+	t_simulation simulation;
+
+	if (parser_args(argc, argv, &simulation))
+		return (1);
+	printf("Number of philo: %d\n", simulation.n_philosophers);
+	printf("Time to die: %d\n", simulation.time_to_die);
+	printf("Time to eat: %d\n", simulation.time_to_eat);
+	printf("Time to sleep: %d\n", simulation.time_to_sleep);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 22:16:53 by advorace          #+#    #+#             */
-/*   Updated: 2026/02/03 22:29:31 by advorace         ###   ########.fr       */
+/*   Updated: 2026/02/04 18:36:52 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,30 @@
 pthread_create - create new thread
 pthread_detach - detach a thread (alternative to pthread_join)
 pthread_join - wait for a thread to finish
-pthread_mutex_init - initialize mutex (lock, each for will be a mutex)
+pthread_mutex_init - initialize mutex (lock, each fork will be a mutex)
 pthread_mutex_destroy - destroy / cleanup a mutex
 pthread_mutex_lock - lock a mutex (acquire the fork, block if already locked by another thread)
 pthread_mutex_unlock - unlock a mutex (release the fork), allows other threads to acquire it
 */
 # include <pthread.h>
+
+typedef struct s_simulation
+{
+	int	n_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+} t_simulation;
+
+// Error functions
+int	wrong_number_format(void);
+int	not_an_int(void);
+int	wrong_number_of_args(void);
+
+// Parsing
+int	parser_args(int argc, char *argv[], t_simulation *t_simulation);
+int	is_int(char *str);
+int	ft_isdigit(int c);
+int	ft_atoi(const char *nptr);
 
 #endif
