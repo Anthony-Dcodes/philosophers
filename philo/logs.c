97a829e31483b38035ddf43c0adfbb67271f26b4
Anthony-Dv <dvoracekantonin@icloud.com>
2026-02-12 20:04:51 +0100

    adjut log_general to tak philosopher insted of simulation

--- philo/logs.c
+++ philo/logs.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   logs.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 22:07:41 by advorace          #+#    #+#             */
-/*   Updated: 2026/02/09 19:38:57 by advorace         ###   ########.fr       */
+/*   Updated: 2026/02/12 20:02:41 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	log_general(t_simulation *simulation, const char *message)
+void	log_general(t_philosopher *philosopher, const char *message)
 {
 	struct timeval	tp;
 	long			timestamp_ms;
-	int				philosopher;
-	
-	pthread_mutex_lock(&simulation->print_mutex);
-	philosopher = simulation->n_philosophers; // Later to fix
+	int				philosopher_n;
+
+	pthread_mutex_lock(&philosopher->sim->print_mutex);
+	philosopher_n = philosopher->id;
 	gettimeofday(&tp, NULL);
 	timestamp_ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-	printf("%ld %d %s\n", timestamp_ms, philosopher, message);
-	pthread_mutex_unlock(&simulation->print_mutex);
+	printf("%ld %d %s\n", timestamp_ms, philosopher_n, message);
+	pthread_mutex_unlock(&philosopher->sim->print_mutex);
 }
