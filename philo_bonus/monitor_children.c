adab90bd01e0765c8cc40c5bea26d254eac32758
Anthony <dvoracekantonin@icloud.com>
2026-04-11 20:40:54 +0200

    monitor_children.c norminette ok

--- philo_bonus/monitor_children.c
+++ philo_bonus/monitor_children.c
@@ -6,29 +6,26 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 09:09:43 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/11 20:13:22 by advorace         ###   ########.fr       */
+/*   Updated: 2026/04/11 20:39:29 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void    monitor_children(pid_t **pids, t_simulation *simulation)
+void	monitor_children(pid_t **pids, t_simulation *simulation)
 {
-    int i;
-    int status;
-    int full_philos;
+	int	i;
+	int	status;
+	int	full_philos;
 
-    i = 0;
-    full_philos = 0;
-    while (full_philos != simulation->n_philosophers)
+	i = 0;
+	full_philos = 0;
+	while (full_philos != simulation->n_philosophers)
 	{
 		if (i == simulation->n_philosophers)
 			i = 0;
-		usleep(100000);
-		//printf("Main process monitoring i: %d, full_philos: %d, n_philos: %d\n", i, full_philos, simulation->n_philosophers);
 		if (waitpid((*pids)[i], &status, WNOHANG) > 0)
 		{
-			//printf("child exited: %d, iterator i: %d, wifexited: %d, wexitstatus: %d, wifsignaled: %d, wtermsig: %d\n", (*pids)[i], i, WIFEXITED(status), WEXITSTATUS(status), WIFSIGNALED(status), WTERMSIG(status));
 			(*pids)[i] = 0;
 			if (WIFEXITED(status) && WEXITSTATUS(status) > 0)
 			{
@@ -40,6 +37,6 @@ void    monitor_children(pid_t **pids, t_simulation *simulation)
 		}
 		++i;
 	}
-    if (full_philos == simulation->n_philosophers)
+	if (full_philos == simulation->n_philosophers)
 		log_all_philosophers_ate(simulation);
 }
