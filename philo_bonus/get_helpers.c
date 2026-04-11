f32c56c0002c38679a6a08286b8145fce79d64c2
Anthony <dvoracekantonin@icloud.com>
2026-04-11 20:02:19 +0200

    get_helpers.c norminette ok

--- philo_bonus/get_helpers.c
+++ philo_bonus/get_helpers.c
@@ -3,51 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   get_helpers.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 10:22:01 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/07 13:25:29 by codespace        ###   ########.fr       */
+/*   Updated: 2026/04/11 20:02:01 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-int get_death(t_simulation *simulation)
+int	get_death(t_simulation *simulation)
 {
-    int value;
+	int	value;
 
-    sem_wait(simulation->state_semaphore);
-    value = simulation->flags.philosopher_died;
-    sem_post(simulation->state_semaphore);
-    return (value);
+	sem_wait(simulation->state_semaphore);
+	value = simulation->flags.philosopher_died;
+	sem_post(simulation->state_semaphore);
+	return (value);
 }
 
-int get_philosopher_full(t_simulation *simulation)
+int	get_philosopher_full(t_simulation *simulation)
 {
-    int value;
+	int	value;
 
-    sem_wait(simulation->state_semaphore);
-    value = simulation->flags.philosopher_full;
-    sem_post(simulation->state_semaphore);
-    return (value);
+	sem_wait(simulation->state_semaphore);
+	value = simulation->flags.philosopher_full;
+	sem_post(simulation->state_semaphore);
+	return (value);
 }
 
-long get_last_meal(t_philosopher *philosopher)
+long	get_last_meal(t_philosopher *philosopher)
 {
-    long value;
+	long	value;
 
-    sem_wait(philosopher->sim->meal_semaphore);
-    value = philosopher->last_meal;
-    sem_post(philosopher->sim->meal_semaphore);
-    return (value);
+	sem_wait(philosopher->sim->meal_semaphore);
+	value = philosopher->last_meal;
+	sem_post(philosopher->sim->meal_semaphore);
+	return (value);
 }
 
-int get_meals_eaten(t_philosopher *philosopher)
+int	get_meals_eaten(t_philosopher *philosopher)
 {
-    int value;
+	int	value;
 
-    sem_wait(philosopher->sim->meal_semaphore);
-    value = philosopher->meals_eaten;
-    sem_post(philosopher->sim->meal_semaphore);
-    return (value);
+	sem_wait(philosopher->sim->meal_semaphore);
+	value = philosopher->meals_eaten;
+	sem_post(philosopher->sim->meal_semaphore);
+	return (value);
 }
