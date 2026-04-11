97b8d3f4a803e89b7ab1c5afa47870c38c357f92
Anthony <dvoracekantonin@icloud.com>
2026-04-11 20:55:23 +0200

    a bit of fix on spawn_children.c

--- philo_bonus/spawn_children.c
+++ philo_bonus/spawn_children.c
@@ -6,59 +6,55 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 11:43:03 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/11 17:52:36 by advorace         ###   ########.fr       */
+/*   Updated: 2026/04/11 20:54:39 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-int spawn_children(t_simulation *simulation, t_philosopher *philosopher, pid_t **pids)
+int	spawn_children(t_simulation *simulation,
+				t_philosopher *philosopher, pid_t **pids)
 {
-    int i;
-    pid_t   pid;
-    int     ret;
+	int		i;
+	pid_t	pid;
+	int		ret;
 
-    i = 0;
-    pid = getpid();
-    ret = ERR_OK;
-    while (i < simulation->n_philosophers && pid != 0) // spawn all child proceses
+	i = 0;
+	pid = getpid();
+	ret = ERR_OK;
+	while (i < simulation->n_philosophers && pid != 0)
 	{
 		pid = fork();
-		//printf("new process spawned: %d, returned pidid: %d, iterator: %d\n", getpid(), (int)(pid), i);
 		if (pid < 0)
-            return (ERR_FORK);
+			return (ERR_FORK);
 		else if (pid == 0)
-            children_execution(philosopher, simulation, i, pids);
+			children_execution(philosopher, simulation, i, pids);
 		else
 			(*pids)[i] = pid;
 		++i;
 	}
-    return (ERR_OK);
+	return (ERR_OK);
 }
 
-void    children_execution(t_philosopher *philosopher, t_simulation *simulation, int i, pid_t **pids)
+void	children_execution(t_philosopher *philosopher,
+						t_simulation *simulation, int i, pid_t **pids)
 {
-    int ret;
+	int	ret;
 
-    ret = ERR_OK;
-    //printf("I am child process! %d\n", getpid());
-    ret = initialize_philosopher_thread(philosopher, i);
-    if (ret != ERR_OK)
-        goto subprocess_exit;
-    monitoring(simulation, philosopher);
-    //printf("Exiting child: %d, id: %d, meals_eaten: %d, to_eat: %d\n", (int)getpid(), philosopher->id, simulation->n_times_must_eat, philosopher->meals_eaten);
-    subprocess_exit:
-        //printf("subprocess_exit started\n");
-		if (philosopher->sim->flags.thread_created)
-		{
-			printf("About to join thread on process: %d, philo id: %d\n", getpid(), philosopher->id);
-			pthread_join(philosopher->thread, NULL);
-		}
-        //subprocess_cleanup(philosopher, *pids);
-		log_end_of_simulation(philosopher, &ret);
-		close_semaphores(philosopher->sim);
-		printf("sems closed\n");
-		free(*pids);
-        //printf("after subprocess_cleanup\n");
-        exit(ret);
+	ret = ERR_OK;
+	ret = initialize_philosopher_thread(philosopher, i);
+	if (ret != ERR_OK)
+		goto subprocess_exit;
+	monitoring(simulation, philosopher);
+	subprocess_exit:
+	if (philosopher->sim->flags.thread_created)
+	{
+		printf("About to join thread on process: %d, philo id: %d\n", getpid(), philosopher->id);
+		pthread_join(philosopher->thread, NULL);
+	}
+	log_end_of_simulation(philosopher, &ret);
+	close_semaphores(philosopher->sim);
+	printf("sems closed\n");
+	free(*pids);
+	exit(ret);
 }
