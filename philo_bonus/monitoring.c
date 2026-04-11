d8f8ed61cf1e7207e5eef62dce9401719f9dc8a5
Anthony <dvoracekantonin@icloud.com>
2026-04-11 20:42:25 +0200

    monitoring.c norminette ok

--- philo_bonus/monitoring.c
+++ philo_bonus/monitoring.c
@@ -3,31 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   monitoring.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 15:55:19 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/09 10:52:26 by codespace        ###   ########.fr       */
+/*   Updated: 2026/04/11 20:41:59 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void    monitoring(t_simulation *simulation, t_philosopher *philosopher)
+void	monitoring(t_simulation *simulation, t_philosopher *philosopher)
 {
-    while (!get_death(simulation) && !get_philosopher_full(simulation))
+	while (!get_death(simulation) && !get_philosopher_full(simulation))
 	{
-		//printf("Monitoring on pid: %d\n", getpid());
 		usleep(1000);
-		//printf("child monitoring timestamp: %ld, meals_eaten: %d, dead: %d, philo_full: %d\n", get_timestamp_ms(), get_meals_eaten(philosopher), get_death(simulation), get_philosopher_full(simulation));
 		death_monitoring(philosopher, simulation);
 		philosopher_full_monitoring(philosopher, simulation);
 	}
-	//printf("child monitoring timestamp: %ld, meals_eaten: %d, dead: %d, philo_full: %d\n", get_timestamp_ms(), get_meals_eaten(philosopher), get_death(simulation), get_philosopher_full(simulation));
 }
 
 void	death_monitoring(t_philosopher *philosopher, t_simulation *sim)
 {
-	long			current_time_ms;
+	long	current_time_ms;
 
 	current_time_ms = get_timestamp_ms();
 	if (current_time_ms - get_last_meal(philosopher) >= sim->time_to_die)
@@ -35,15 +32,14 @@ void	death_monitoring(t_philosopher *philosopher, t_simulation *sim)
 	return ;
 }
 
-void	philosopher_full_monitoring(t_philosopher *philosopher, t_simulation *sim)
+void	philosopher_full_monitoring(t_philosopher *philosopher,
+									t_simulation *sim)
 {
-	int				meals_eaten;
-	int				n_times_must_eat;
-
+	int	meals_eaten;
+	int	n_times_must_eat;
 
 	n_times_must_eat = sim->n_times_must_eat;
 	meals_eaten = get_meals_eaten(philosopher);
-	//printf("philo id: %d, meals eaten: %d, need to eat: %d\n", philosopher->id, meals_eaten, n_times_must_eat);
 	if (meals_eaten >= n_times_must_eat && n_times_must_eat)
 		set_philosopher_full(philosopher->sim);
 	return ;
