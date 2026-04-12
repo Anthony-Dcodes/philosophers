23e97eac5496f0c76d49c04eda33e5c095cf4cab
Anthony <dvoracekantonin@icloud.com>
2026-04-12 11:52:16 +0200

    structs.h norminette ok

--- philo_bonus/structs.h
+++ philo_bonus/structs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 08:57:09 by codespace         #+#    #+#             */
-/*   Updated: 2026/04/09 11:38:12 by codespace        ###   ########.fr       */
+/*   Updated: 2026/04/12 11:51:58 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 typedef struct s_flags
 {
 	int	philosopher_died;
-	int philosopher_full;
+	int	philosopher_full;
 	int	thread_created;
 	int	pids_mallocked;
-} t_flags;
+}	t_flags;
 
 typedef struct s_simulation
 {
-	int	n_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	n_times_must_eat;
+	int		n_philosophers;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		n_times_must_eat;
 	sem_t	*print_semaphore;
 	sem_t	*state_semaphore;
 	sem_t	*fork_semaphore;
@@ -34,13 +34,13 @@ typedef struct s_simulation
 	sem_t	*end_simulation_semaphore;
 	sem_t	*seats_semaphore;
 	t_flags	flags;
-} t_simulation;
+}	t_simulation;
 
 typedef struct s_philosopher
 {
-	int	id;
-	long	last_meal;
-	int		meals_eaten;
+	int				id;
+	long			last_meal;
+	int				meals_eaten;
 	t_simulation	*sim;
-	pthread_t	thread;
-} t_philosopher;
+	pthread_t		thread;
+}	t_philosopher;
