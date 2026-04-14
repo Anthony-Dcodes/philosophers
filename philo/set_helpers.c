ead5fed51835a5c25c596cebf39ef56e500e4f70
AnthonyDcodes <85177751+Anthony-Dcodes@users.noreply.github.com>
2026-04-14 14:59:20 +0000

    set_helpers.c norminette ok

--- philo/set_helpers.c
+++ philo/set_helpers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_helpers.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 16:11:47 by codespace         #+#    #+#             */
-/*   Updated: 2026/03/08 13:07:35 by advorace         ###   ########.fr       */
+/*   Updated: 2026/04/14 14:58:57 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	set_last_meal_time(t_philosopher *philosopher)
 {
-	long			timestamp_ms;
+	long	timestamp_ms;
 
 	pthread_mutex_lock(&philosopher->meal_mutex);
 	timestamp_ms = get_timestamp_ms();
@@ -29,16 +29,16 @@ void	set_death(t_simulation *simulation, int id)
 	pthread_mutex_unlock(&simulation->state_mutex);
 }
 
-void    set_all_philosophers_full(t_simulation *simulation)
+void	set_all_philosophers_full(t_simulation *simulation)
 {
-    pthread_mutex_lock(&simulation->state_mutex);
-    simulation->flags.all_philosophers_full = 1;
-    pthread_mutex_unlock(&simulation->state_mutex);
+	pthread_mutex_lock(&simulation->state_mutex);
+	simulation->flags.all_philosophers_full = 1;
+	pthread_mutex_unlock(&simulation->state_mutex);
 }
 
-void    set_increment_meals_eaten(t_philosopher *philosopher)
+void	set_increment_meals_eaten(t_philosopher *philosopher)
 {
-    pthread_mutex_lock(&philosopher->meal_mutex);
-    ++philosopher->meals_eaten;
-    pthread_mutex_unlock(&philosopher->meal_mutex);
+	pthread_mutex_lock(&philosopher->meal_mutex);
+	++philosopher->meals_eaten;
+	pthread_mutex_unlock(&philosopher->meal_mutex);
 }
